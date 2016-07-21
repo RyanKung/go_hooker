1c12350277c29fb4c3d32d9237233576fc853ea2
Ryan.K <ryankung@ieee.org>
2016-07-21 11:07:10 +0800

    fixed path

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 cp *.mobileprovision Payload/pokemongo.app/embedded.mobileprovision
-cp LatestBuild/PokemonHook.dylib Payload/pokemongo.app/libLocationFaker.dylib
+cp LatestBuild/LocationFaker.dylib Payload/pokemongo.app/libLocationFaker.dylib
 rm -rf pokemon.ipa
 zip -r pokemon.ipa Payload
 /usr/local/lib/ruby/gems/2.2.0/gems/sigh-1.8.0/lib/assets/resign.sh pokemon.ipa "iPhone Developer: ZHEN KONG (7LB2BBJ2SN)" -p Payload/pokemongo.app/embedded.mobileprovision pokemon.ipa
